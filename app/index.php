92f8cd2cd3eb57c6d14874a142460d925eac7885
unknown <jrodicio98@gmail.com>
2021-03-29 11:52:22 -0300

    test 02

--- app/index.php
+++ app/index.php
@@ -1,15 +1,17 @@
 <?php
-error_reporting(-1);
-ini_set('display_errors', 1);
+    error_reporting(-1);
+    ini_set('display_errors', 1);
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
-use Slim\Routing\RouteCollectorProxy;
+    use Psr\Http\Message\ResponseInterface as Response;
+    use Psr\Http\Message\ServerRequestInterface as Request;
+    use Slim\Factory\AppFactory;
+    use Slim\Routing\RouteCollectorProxy;
 
-require __DIR__ . '/../vendor/autoload.php';
+    require __DIR__ . '/../vendor/autoload.php';
+
+    $ficheros = scandir("./");
+
+    print_r($ficheros);
 
 
-
-print("ASDASDASDAS");
 ?>
